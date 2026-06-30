973ed1184f1d7a73f4f66c31ecc6dd6b5dcce65a
j0shuajun <kimmj1e34@naver.com>
2026-06-30 16:36:35 +0900

    add tests for `get_sum()` and `get_sum_sum()`

--- test_main.py
+++ test_main.py
@@ -1,5 +1,12 @@
 import pytest
+from main import Calc
 
-def test_sample():
-    assert 1 == 1
-    pytest.fail()
+
+def test_get_sum():
+    calc = Calc()
+    assert calc.get_sum(1, 2) == 3
+
+
+def test_get_sum_sum():
+    calc = Calc()
+    assert calc.get_sum_sum(1, 2, 3) == 6
